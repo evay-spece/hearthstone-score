50811ab992252f8258c859934b7dec87b8226b1e
evay-spece <chenhuangevay@gmail.com>
2026-09-23 00:23:36 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C="hs-score-compact-v1",F=["./","./index.html","./manifest.json"];
+const C="hs-score-responsive-v1",F=["./","./index.html","./manifest.json"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(F))));
 self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
 self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
