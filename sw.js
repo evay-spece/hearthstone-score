d42acda322f37cad6e66098bb11e1e771142d865
evay-spece <chenhuangevay@gmail.com>
2026-09-25 16:33:54 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "hs-score-v1-1-4";
+const CACHE_NAME = "hs-score-v1-1-5";
 const APP_SHELL = ["./", "./index.html", "./manifest.json"];
 
 self.addEventListener("install", event => {
